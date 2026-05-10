17815d1e7e4dbe9c6ca39dafe6fa38d0a490ee92
Kalendaria <143078128+Kalendaria@users.noreply.github.com>
2026-05-10 11:54:47 +0700

    fix server

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'moneypad-production.up.railway.app', // ← แทนด้วย URL จริงจาก Vercel
+    'https://money-pad.vercel.app/', // ← แทนด้วย URL จริงจาก Vercel
   ],
   credentials: true,
 }));
